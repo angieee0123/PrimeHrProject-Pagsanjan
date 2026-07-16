be632df8f26d5ced4352b1dc67a625e878b842d8
angieee123 <angelicaxxcuevas@gmail.com>
2026-07-16 17:11:16 +0800

    fix: correct HTML entity for ampersand in topbar title

--- primeHrMagdalenaLaravel/resources/views/admin/topbar/leaveandbenefitsTopbar.blade.php
+++ primeHrMagdalenaLaravel/resources/views/admin/topbar/leaveandbenefitsTopbar.blade.php
@@ -1,4 +1,4 @@
-<x-topbar title="Leave &amp; Benefits Management">
+<x-topbar title="Leave & Benefits Management">
     <x-slot:icon><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/></x-slot:icon>
     <x-slot:subtitle>{{ now()->format('l, F j, Y') }} &nbsp;·&nbsp; Municipal Government of Pagsanjan</x-slot:subtitle>
     <x-slot:actions>
